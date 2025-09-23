5fdec3e1a919daee881603cb8762cb777f7b3873
AlumHub <info@thealumhub.com>
2025-09-23 00:33:33 -0400

    added shell scripts

---
+++ pdebench/data_gen/data_gen_NLE/CompressibleFluid/run_trainset_1D_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+nn=1
+key=2020
+while [ "$nn" -le 10 ]; do
+ CUDA_VISIBLE_DEVICES='0,1' python3 /content/PDEBench/pdebench/data_gen/data_gen_NLE/CompressibleFluid/CFD_multi_Hydra.py +args=1D_Multi_1.yaml ++args.init_key="$key"
+ nn=$((nn + 1))
+ key=$((key + 1))
+ echo "$nn"
+ echo "$key"
+done
