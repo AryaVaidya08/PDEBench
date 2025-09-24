707f449cc417469b24f11fddf734a5df2e348b9b
AlumHub <info@thealumhub.com>
2025-09-23 22:53:09 -0400

    added 2d configs

---
+++ pdebench/data_gen/data_gen_NLE/CompressibleFluid/run_trainset_2D_4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+nn=1
+key=2031
+while [ "$nn" -le 100 ]; do
+  CUDA_VISIBLE_DEVICES='0,1,2,3' python3 /content/PDEBench/pdebench/data_gen/data_gen_NLE/CompressibleFluid/CFD_multi_Hydra.py +args=2D_Multi_Rand_4.yaml ++args.init_key="$key"
+  #CUDA_VISIBLE_DEVICES='0,1,2,3' python3 CFD_multi_Hydra.py +args=2D_Multi_Rand_HR.yaml ++args.init_key="$key"
+  nn=$((nn + 1))
+  key=$((key + 1))
+  echo "$nn"
+  echo "$key"
+done
